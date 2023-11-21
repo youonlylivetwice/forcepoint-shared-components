08b18e05adab959ed35d6557dc9fe1388cc0ff09
Marvin J. Cortés <marvin.cortes@gmail.com>
2023-11-20 18:38:19 -0600

    feat: add spacing

--- src/lib/components/01-elements/button/button.tsx
+++ src/lib/components/01-elements/button/button.tsx
@@ -7,7 +7,7 @@ export default function Button({ text, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="bg-navy hover:bg-teal rounded-full px-4 py-3 text-white disabled:bg-black"
+      className="bg-navy hover:bg-teal rounded-full px-lg py-sm text-white disabled:bg-black"
     >
       {text}
     </button>
