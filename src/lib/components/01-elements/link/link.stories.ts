4921dceecba8e763f4cada30173137a6ea380736
Roberto Hernandez <roberthb94@gmail.com>
2023-12-20 09:35:30 -0600

    feat(336): adjusting the link component

--- src/lib/components/01-elements/link/link.stories.ts
+++ src/lib/components/01-elements/link/link.stories.ts
@@ -2,42 +2,52 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import Link from './link';
 
+const svgIcons = import.meta.glob('../../../../../assets/images/icons/*.svg');
+const icons = [];
+
+for (const key in svgIcons) {
+  const icon = key.split('icons/')[1].split('.svg')[0];
+  icons.push(icon);
+}
+
 const meta = {
   title: 'Elements/Link',
   component: Link,
-  tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    icon: {
+      options: ['none', ...icons],
+      control: { type: 'select' },
+    },
+    iconPosition: {
+      options: ['right', 'left'],
+      control: { type: 'select' },
+    },
+    variant: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } satisfies Meta<typeof Link>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    text: 'Link',
+    icon: 'play',
+    iconPosition: 'left',
+    text: 'Watch the Video',
     url: 'http://localhost:6006/',
+    disabled: false,
   },
 };
 
-export const Secondary: Story = {
+export const Underline: Story = {
   args: {
-    text: 'Link',
+    text: 'Get It Today',
     url: 'http://localhost:6006/',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    text: 'Large',
-    url: 'http://localhost:6006/',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    text: 'Small',
-    url: 'http://localhost:6006/',
+    variant: 'underline',
+    disabled: false,
   },
 };
--- src/lib/components/01-elements/link/link.tsx
+++ src/lib/components/01-elements/link/link.tsx
@@ -1,13 +1,47 @@
+import Icon from "../icon/icon";
+
 export type LinkProps = {
-  text: string;
   url: string;
+  text: string;
+  icon?: string;
+  disabled?: boolean;
+  iconPosition?: 'right' | 'left';
+  variant?: 'underline' | 'default';
   size?: 'small' | 'medium' | 'large';
 };
 
-export default function Link({ text, url, ...props }: LinkProps) {
+export default function Link({ text, url, icon, variant, iconPosition = 'right', disabled, ...props }: LinkProps) {
+  const getVariantClasses = () => {
+    switch (variant) {
+      case 'underline':
+        return 'underline underline-offset-8';
+      default:
+        return '';
+    }
+  };
+
+  const getConditionalClasses = () => {
+    if (disabled) {
+      return 'text-black opacity-60 pointer-events-none';
+    } else {
+      return 'text-navy hover:text-teal cursor-pointer';
+    }
+  };
+
+  const renderedIcon = () => {
+    if (icon && icon !== 'none') {
+      return <Icon name={icon} className="w-md h-md" />;
+    }
+  };
+
+  const variantClasses = getVariantClasses();
+  const conditionalClasses = getConditionalClasses();
+
   return (
-    <a {...props} className="text-crimson text-3xl font-bold" href={url}>
+    <a {...props} className={`inline-flex gap-sm items-center font-bold text-h4 ${variantClasses} ${conditionalClasses}`} href={url}>
+      {iconPosition === 'left' && renderedIcon()}
       {text}
+      {iconPosition === 'right' && renderedIcon()}
     </a>
   );
 }
