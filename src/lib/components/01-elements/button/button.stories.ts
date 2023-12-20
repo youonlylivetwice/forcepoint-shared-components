43bb0d797fb79cbee347f23e78f4c79966ac133f
Roberto Hernandez <roberthb94@gmail.com>
2023-12-20 09:35:09 -0600

    feat(336): adjusting the button component

--- src/lib/components/01-elements/button/button.stories.ts
+++ src/lib/components/01-elements/button/button.stories.ts
@@ -2,11 +2,36 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import Button from './button';
 
+const svgIcons = import.meta.glob('../../../../../assets/images/icons/*.svg');
+const icons = [];
+
+for (const key in svgIcons) {
+  const icon = key.split('icons/')[1].split('.svg')[0];
+  icons.push(icon);
+}
+
 const meta = {
   title: 'Elements/Button',
   component: Button,
-  tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    size: {
+      options: ['small', 'medium', 'large'],
+      control: { type: 'select' },
+    },
+    variant: {
+      table: {
+        disable: true,
+      },
+    },
+    icon: {
+      options: ['none', ...icons],
+      control: { type: 'select' },
+    },
+    iconPosition: {
+      options: ['right', 'left'],
+      control: { type: 'select' },
+    },
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -14,26 +39,21 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    text: 'Button text',
+    text: 'Button label',
+    icon: 'arrow-right',
+    iconPosition: 'right',
+    size: 'medium',
+    disabled: false,
   },
 };
 
-export const Disabled: Story = {
+export const Outline: Story = {
   args: {
-    text: 'Button text',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    text: 'Large button text',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    text: 'Small button text',
+    text: 'Button label',
+    icon: 'arrow-right',
+    iconPosition: 'right',
+    variant: 'outline',
+    size: 'medium',
+    disabled: false,
   },
 };
--- src/lib/components/01-elements/button/button.tsx
+++ src/lib/components/01-elements/button/button.tsx
@@ -1,15 +1,58 @@
+import Icon from "../icon/icon";
+
 export type ButtonProps = {
   text: string;
+  icon?: string;
+  disabled?: boolean;
+  variant?: 'solid' | 'outline';
+  iconPosition?: 'right' | 'left';
   size?: 'small' | 'medium' | 'large';
 };
 
-export default function Button({ text, ...props }: ButtonProps) {
+export default function Button({ text, variant, size, icon, iconPosition = 'right', ...props }: ButtonProps) {
+  const getFontSizeClasses = () => {
+    switch (size) {
+      case 'small':
+        return 'text-h5 px-md py-[0.5rem]';
+      case 'large':
+        return 'text-h4 px-lg py-[1rem]';
+      default:
+        return 'text-h5 px-lg py-sm';
+    }
+  };
+
+  const getVariantClasses = () => {
+    switch (variant) {
+      case 'outline':
+        return 'border-navy border-2 text-black hover:border-teal disabled:border-black disabled:opacity-60';
+      default:
+        return 'bg-navy text-white hover:bg-teal disabled:bg-black disabled:opacity-60';
+    }
+  };
+
+  const getConditionalClasses = () => {
+    if (props.disabled) {
+      return 'cursor-not-allowed';
+    } else {
+      return 'cursor-pointer';
+    }
+  };
+
+  const variantClasses = getVariantClasses();
+  const fontSizeClasses = getFontSizeClasses();
+  const conditionalClasses = getConditionalClasses();
+
+  const renderedIcon = () => {
+    if (icon && icon !== 'none') {
+      return <Icon name={icon} className="w-sm h-sm" />;
+    }
+  };
+
   return (
-    <button
-      {...props}
-      className="rounded-full bg-navy px-lg py-sm text-white hover:bg-teal disabled:bg-black"
-    >
+    <button {...props} className={`inline-flex gap-sm items-center rounded-lg ${variantClasses} ${fontSizeClasses} ${conditionalClasses}`}>
+      {iconPosition === 'left' && renderedIcon()}
       {text}
+      {iconPosition === 'right' && renderedIcon()}
     </button>
   );
 }
