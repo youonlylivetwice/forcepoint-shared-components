79d421321c8ef7809ac0765b4ecadfab1c5369c8
Roberto Hernandez <roberthb94@gmail.com>
2023-12-20 09:33:08 -0600

    feat(336): story page dedicated to sizes

---
+++ src/lib/components/00-tokens/sizes/sizes.tsx
@@ -0,0 +1,30 @@
+import { TokensType } from "../types";
+
+export type SpacingProps = {
+  widths: TokensType;
+  heights: TokensType;
+};
+
+export default function Sizes({ widths, heights }: SpacingProps) {
+  const renderItem = ([key, value]: [string, string], index: number) => {
+    return (
+      <li key={index} className="flex flex-row items-center gap-md">
+        <div className="flex flex-row gap-md">
+          <p className="w-xxl text-md text-grey">size.{value.replace('w-', '')}</p>
+          <p className="w-xl text-md text-grey">{key}</p>
+        </div>
+        {/* Swatch */}
+        <div className={`flex gap-y-sm w-max bg-violette ${widths[key]} ${heights[key]}`}></div>
+      </li>
+    );
+  }
+
+  return (
+    <div className="p-md">
+      <h2 className="text-h3 mb-lg">Sizes</h2>
+      <ul className="flex flex-col gap-md">
+        {Object.entries(widths as TokensType).map(renderItem)}
+      </ul>
+    </div>
+  );
+}
