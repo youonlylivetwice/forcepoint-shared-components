b87b34dd6a559d8a350c83154d88558a7164e0a5
josue2591 <josuegv2591@gmail.com>
2024-01-12 00:00:51 +0100

    feat(fp-336) temp icons file

---
+++ src/lib/components/02-components/icons/icons.stories.tsx
@@ -0,0 +1,12 @@
+// Temporary file to avoid Storybook error
+import type { Meta } from '@storybook/react';
+
+const meta = {
+  title: 'Elements/Icons',
+} satisfies Meta;
+
+export default meta;
+
+export const Default = () => {
+  return <h2>Icons</h2>;
+};
