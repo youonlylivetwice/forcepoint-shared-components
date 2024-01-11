23528bd1f49ce35cd2efc204ea3814150506d60e
Roberto Hernandez <roberthb94@gmail.com>
2024-01-11 14:45:26 -0600

    feat(336): joining storybook link pages

--- src/lib/components/01-elements/link/link.stories.tsx
+++ src/lib/components/01-elements/link/link.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import PlayIcon from "../../../assets/img/icons/play.svg?react";
+import PlayIcon from '../../../assets/img/icons/play.svg?react';
 
 import Link from './link';
 
@@ -42,17 +42,7 @@ export const Default: Story = {
     url: 'http://localhost:6006/',
     color: 'navy',
     size: 'default',
-    disabled: false,
-  },
-};
-
-export const Underline: Story = {
-  args: {
-    text: 'Underline Link',
-    url: 'http://localhost:6006/',
-    variant: 'underline',
-    color: 'navy',
-    size: 'default',
+    variant: 'default',
     disabled: false,
   },
 };
