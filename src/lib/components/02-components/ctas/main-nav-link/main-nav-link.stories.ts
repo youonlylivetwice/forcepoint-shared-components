d319d4456c345dae8263c53b1da59acd4c76fbef
Roberto Hernandez <roberthb94@gmail.com>
2024-02-12 12:26:03 -0600

    feat(349): add main nav link component

---
+++ src/lib/components/02-components/ctas/main-nav-link/main-nav-link.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MainNavLink from './main-nav-link';
+
+const meta = {
+  title: 'Components/Ctas/Main Nav link',
+  component: MainNavLink,
+} satisfies Meta<typeof MainNavLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Why Forcepoint',
+  },
+};
