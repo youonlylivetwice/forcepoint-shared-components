39f0416fa8b2b1265374870e804d96ea127ac20a
Gustavo Nering <gustavo.nering@bairesdev.com>
2024-03-12 14:29:09 -0300

    Create image component
    * create image component
    * create story for dark and light theme

---
+++ src/lib/components/01-elements/img/img.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+
+import Img from './img';
+
+
+const meta = {
+  title: 'Elements/Image',
+  component: Img,
+  argTypes: {
+    imgPath: {
+      control: { type: 'text' },
+    },
+    imgDesc: {
+      control: { type: 'text' },
+    },
+  },
+} satisfies Meta<typeof Img>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    imgPath: 'https://via.placeholder.com/150',
+    imgDesc: 'Placeholder image',
+  },
+};
+
+export const DarkTheme: Story = {
+  args: {
+    imgPath: 'https://via.placeholder.com/150',
+    imgDesc: 'Placeholder image',
+    theme: 'dark',
+  },
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+};
---
+++ src/lib/components/01-elements/img/img.tsx
@@ -0,0 +1,22 @@
+import Typography from "../typography/typography";
+
+type ImgProps = {
+  imgPath: string;
+  imgDesc: string;
+  theme?: 'dark' | 'light';
+};
+
+export default function Img({imgPath, imgDesc, theme = 'light'}: ImgProps) {
+  return (
+    <figure>
+      <img src={imgPath} alt={imgDesc} />
+      <figcaption>
+        <Typography
+          variant='body-4'
+          className={theme === 'dark' ? 'text-azure' : 'text-black'}>
+            {imgDesc}
+        </Typography>
+      </figcaption>
+    </figure>
+  );
+}
