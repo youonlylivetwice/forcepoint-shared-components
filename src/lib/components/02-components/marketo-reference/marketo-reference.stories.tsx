9d7f10f5f3c98c351928e4a9d8e3f67aa9fe72a6
Marvin J. Cortés <marvin.cortes@gmail.com>
2024-06-19 09:17:03 -0600

    feat(fp-921): add marketo reference contaier styles

--- src/lib/components/02-components/marketo-reference/marketo-reference.stories.tsx
+++ src/lib/components/02-components/marketo-reference/marketo-reference.stories.tsx
@@ -20,7 +20,7 @@ export const Default: Story = {
   args: {
     marketoForm: renderedForm,
     image: {
-      src: 'https://forcepoint-drupal.lndo.site/sites/default/files/final_plea_opt_10_55.jpg',
+      src: 'https://s3-alpha-sig.figma.com/img/5c5a/5fe1/f5933be84d46d10d140685b72adc7a6a?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PtwQLu0urBuJTj4zJKijyMnLHKnnDO50WALlHZD7xxMECX64DB2Js6MmRTpTVrDyHlXiROtUSlHyPFe9m1L4CvO09EbrkhwIhot2m6PY9Au3f6TfcM8orK6-DzQqzSAhqWCVfeVkI5gG2ryDAUC6t3uoIf86CLq-raF~h3c~wnl4Ee~RnYovtEsKYC3dDuRWFWu42g3A2Hwl0wRwXcC3txzOF7l42FeKGifmO-EUvkBg80FpcIKG8CK7GHb3qMNHqyeDjr4TXUwbwMxVx~Xdh4M-XlnjGazknG1ndfT1HlumMvfWcuvwtpZASqhfCZlDSMOKejv6M6KH3~ooAo1S5g__',
       alt: 'image',
     },
     bgColor: 'azure',
