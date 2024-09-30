e1d5ea5dbc20fcb4c0879551ddecb42b653c879f
Gustavo Nering <gustavo.nering@bairesdev.com>
2024-09-30 17:03:45 -0300

    scroll test

--- src/lib/components/02-components/table/table-row.tsx
+++ src/lib/components/02-components/table/table-row.tsx
@@ -1,7 +1,7 @@
-import { setContrastTextColor } from '../../00-tokens/color/color-shared';
 import React, { ReactNode, useRef } from 'react';
-import useMediaQuery from '../../../hooks/use-media-query';
-import { useDataTable } from './data-table-provider';
+import { setContrastTextColor } from '../../00-tokens/color/color-shared';
+// import useMediaQuery from '../../../hooks/use-media-query';
+// import { useDataTable } from './data-table-provider';
 
 export interface TableRowProps {
   bgColor?: string;
@@ -11,41 +11,40 @@ export interface TableRowProps {
 const TableRow = ({ bgColor, children }: TableRowProps) => {
   let rowStyles: React.CSSProperties = {};
   const row = useRef<HTMLTableRowElement>(null);
-  const { currentPage, itemsPerPage } = useDataTable();
-  const isMobile = useMediaQuery('(max-width: 59.374rem)');
+  // const { currentPage, itemsPerPage } = useDataTable();
+  // const isMobile = useMediaQuery('(max-width: 59.374rem)');
 
-  function currentPageAnimation() {
-    if (row && row.current) {
-      const tableElement = row.current.closest('table');
+  // function currentPageAnimation() {
+  //   if (row && row.current) {
+  //     const tableElement = row.current.closest('table');
 
-      if (tableElement) {
-        // Trying to retrieve the target element for the current page.
-        const targetElementIndex = (currentPage - 1) * itemsPerPage;
-        const targetElement = tableElement.rows[targetElementIndex];
+  //     if (tableElement) {
+  //       // Trying to retrieve the target element for the current page.
+  //       const targetElementIndex = (currentPage - 1) * itemsPerPage;
+  //       const targetElement = tableElement.rows[targetElementIndex];
 
-        targetElement?.scrollIntoView({
-          behavior: 'smooth',
-          inline: 'start',
-          block: 'nearest',
-        });
-      }
-    }
-  }
+  //       targetElement?.scrollIntoView({
+  //         behavior: 'smooth',
+  //         inline: 'start',
+  //         block: 'nearest',
+  //       });
+  //     }
+  //   }
+  // }
 
   if (bgColor) {
     rowStyles = setContrastTextColor(bgColor);
   }
 
-  if (isMobile) {
-    currentPageAnimation();
-  }
+  // if (isMobile) {
+  //   currentPageAnimation();
+  // }
 
   return (
     <tr
       className="bg-white max-md:flex max-md:flex-col"
       style={rowStyles}
-      ref={row}
-    >
+      ref={row}>
       {children}
     </tr>
   );
