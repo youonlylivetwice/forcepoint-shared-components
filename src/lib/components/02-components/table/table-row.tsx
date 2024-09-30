0200c1442e3a9da391d38de89c2218f1caaa1084
Gustavo Nering <gustavo.nering@bairesdev.com>
2024-09-30 17:47:08 -0300

    Wrap scroll fn inside useEffect

--- src/lib/components/02-components/table/table-row.tsx
+++ src/lib/components/02-components/table/table-row.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
+import useMediaQuery from '../../../hooks/use-media-query';
 import { setContrastTextColor } from '../../00-tokens/color/color-shared';
-// import useMediaQuery from '../../../hooks/use-media-query';
-// import { useDataTable } from './data-table-provider';
+import { useDataTable } from './data-table-provider';
 
 export interface TableRowProps {
   bgColor?: string;
@@ -11,38 +11,38 @@ export interface TableRowProps {
 const TableRow = ({ bgColor, children }: TableRowProps) => {
   let rowStyles: React.CSSProperties = {};
   const row = useRef<HTMLTableRowElement>(null);
-  // const { currentPage, itemsPerPage } = useDataTable();
-  // const isMobile = useMediaQuery('(max-width: 59.374rem)');
-
-  // function currentPageAnimation() {
-  //   if (row && row.current) {
-  //     const tableElement = row.current.closest('table');
-
-  //     if (tableElement) {
-  //       // Trying to retrieve the target element for the current page.
-  //       const targetElementIndex = (currentPage - 1) * itemsPerPage;
-  //       const targetElement = tableElement.rows[targetElementIndex];
-
-  //       targetElement?.scrollIntoView({
-  //         behavior: 'smooth',
-  //         inline: 'start',
-  //         block: 'nearest',
-  //       });
-  //     }
-  //   }
-  // }
+  const { currentPage, itemsPerPage } = useDataTable();
+  const isMobile = useMediaQuery('(max-width: 59.374rem)');
 
   if (bgColor) {
     rowStyles = setContrastTextColor(bgColor);
   }
+  useEffect(() => {
+    function currentPageAnimation() {
+      if (row && row.current) {
+        const tableElement = row.current.closest('table');
 
-  // if (isMobile) {
-  //   currentPageAnimation();
-  // }
+        if (tableElement) {
+          // Trying to retrieve the target element for the current page.
+          const targetElementIndex = (currentPage - 1) * itemsPerPage;
+          const targetElement = tableElement.rows[targetElementIndex];
+
+          targetElement?.scrollIntoView({
+            behavior: 'smooth',
+            inline: 'start',
+            block: 'nearest',
+          });
+        }
+      }
+    }
+    if (isMobile) {
+      currentPageAnimation();
+    }
+  }, [isMobile, currentPage, itemsPerPage]);
 
   return (
     <tr
-      className="bg-white max-md:flex max-md:flex-col"
+      className="overflow-x-auto bg-white max-md:flex max-md:flex-col"
       style={rowStyles}
       ref={row}>
       {children}
