759c8c2cd8e4923313df8ca04e5229ee9578b084
Gustavo Nering <gustavo.nering@forcepoint.com>
2026-08-27 11:20:39 -0300

    remove unused import

--- src/lib/components/02-components/table/table.tsx
+++ src/lib/components/02-components/table/table.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useRef } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 import Typography from '../../01-elements/typography/typography';
 import { useDataTable } from './data-table-provider';
 import TablePager from './table-pager';
